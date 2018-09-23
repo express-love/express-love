f6e1569434fc0234528fb3c6b2a3259ea6947a74
First Last <jedmao@users.noreply.github.com>
2018-09-23 16:33:12 -0500

    remove jsdocs from TS defs

--- packages/functional-middleware/index.d.ts
+++ packages/functional-middleware/index.d.ts
@@ -12,33 +12,10 @@ export interface IResponse {
   headers?: ResponseHeaders;
   status: number;
 }
-/**
- * Creates an express middleware function that allows endpoints to be expressed
- * as a function returning a response value. The specified `createResponse`
- * function will be called and the response value that is returned will be sent
- * to the specified `sendResponse` function.
- *
- * @example
- * import * as express from 'express';
- * import functionalMiddleware from '@express-love/functional-middleware';
- *
- * const sendResponse = (res, response) => res.status(response.status).send(response.body);
- * const apiHandler = (createResponse) => functionalMiddleware({ createResponse, sendResponse });
- *
- * const app = express();
- *
- * app.get('/api/examples', apiHandler(async (req) => {
- *   await doSomething();
- *   return { status: 200, body: 'hello world' };
- * }));
- */
 export default function functionalMiddleware({
   createResponse,
   sendResponse,
 }: {
   createResponse: (req?: Request) => Promise<IResponse>;
-  /**
-   * Sends the created response object.
-   */
   sendResponse: (res?: Response, response?: IResponse) => void;
 }): RequestHandler;
--- packages/map-values-async/index.d.ts
+++ packages/map-values-async/index.d.ts
@@ -1,26 +1,4 @@
-/**
- * Creates a shallow clone of the specified `source` object. Any properties of
- * the source object that are promises will be replaced with the resolved value.
- *
- * @example
- * const result = await mapValuesAsync({
- *   a: Promise.resolve('apples'),
- *   b: Promise.resolve('bananas'),
- *   c: 'corn',
- * });
- *
- * expect(result).toEqual({
- *   a: 'apples',
- *   b: 'bananas',
- *   c: 'corn',
- * });
- *
- * @returns A clone of `source` with the promise properties replaced with their resolved values.
- */
 export default function mapValuesAsync(
-  /**
-   * An object with properties that might be promises.
-   */
   source: {
     [key: string]: any;
   },
